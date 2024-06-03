7c633ca64d8618637c9526daa87a3950bbbeff5b
AntonBear <holzed15@gmail.com>
2024-06-03 19:43:08 +0300

    gogogooggoo

---
+++ task2.js
@@ -0,0 +1,15 @@
+// Напишите функцию isEmpty(obj), которая возвращает true, если у объекта нет свойств, иначе false. 169
+
+let schedule = {}
+
+function isEmpty(obj) {
+  for (let key in obj) {
+    // ошибка - забыл указать let для key
+    return false // ошибка - указал true
+  }
+  return true // ошибка - указал false
+}
+
+console.log(isEmpty(schedule))
+schedule['8:30'] = 'get up'
+console.log(isEmpty(schedule))
